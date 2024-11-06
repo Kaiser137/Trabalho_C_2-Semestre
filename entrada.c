ef99c033524b852eb62857854c5fece9fa986696
Cezar Vitor <165351269+Kaiser137@users.noreply.github.com>
2024-11-06 19:18:29 -0300

    Update entrada.c
    Entrada de Dados OK

--- entrada.c
+++ entrada.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
 
 int main(){
-    int alunos;
+    int alunos, quantidade_teste, enquanto = 0;
 
+    while(enquanto < 1){
     printf("Digite a quantidade de alunos desejado: ");
-    scanf("%d", &alunos);
+    scanf("%d", &quantidade_teste);
+    if(quantidade_teste > 30 || quantidade_teste < 0){
+        printf("Numero de alunos maior ou insuficiente!");
+    }
+    else{
+        alunos = quantidade_teste;
+        enquanto++;
+    }
+    }
 
-    int matriz[4][alunos];
-    for(int i = 0; i < 1; i++){
-        for(int j = 0; j < alunos){
+    float matriz[4][alunos];
+    int linha = 0;
+        for(int j = 0; j < alunos; j++){
             printf("Digite o ID do aluno: ");
-            scanf("%d", &matriz[i][j]);
+            scanf("%f", &matriz[linha][j]);
+        }
+
+
+    for(int i = 0; i < alunos; i++){
+        for(int j = 1; j < 4; j++){
+            printf("Digite a %d nota do aluno %d: ", j, (int)matriz[0][i]);
+            scanf("%f", &matriz[j][i]);
+        }
+    }
+
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < alunos; j++){
+            printf("A matriz na posicao [%d][%d] eh: %.2lf\n", i, j, matriz[i][j]);
         }
     }
 }
